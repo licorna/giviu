a0a0d68fd160985d2455bdd6a1d7d36506ad6f83
Sebastian Durán <yo@sebaduran.com>
2013-12-24 12:35:57 -0300

    modificar orden de funciones en registro/login
    Una vez recibido el ajax revisa si es 200 o 400 y después recién muestra la info.

--- giviu/giviu/static/js/facebookconnect.js
+++ giviu/giviu/static/js/facebookconnect.js
@@ -29,45 +29,46 @@
     FB.api('/me/', function(response) {
       $.ajax({
         type: "GET",
-        url: "/api/users/exists-by-fb/"+response.id,
+        url: "/api/users/exists-by-fb/s"+response.id,
         cache: false,
         statusCode: {
           200: function() {
             $('#register').append('<input type="hidden" name="facebookId" value="'+response.id+'">');
-            $('#facebook-name').html('<strong>Bienvenido nuevamente</strong> '+response.first_name);
+            $('#facebook-name').html('<strong>Bienvenido nuevamente</strong> <br/>'+response.first_name);
+            showInfo();
           },
           404:function(){
             if(response.first_name==undefined){
               first_name = '';
             }else{
               first_name = response.first_name;
-            }            
+            }
             if(response.location.name==undefined){
               location_name = '';
             }else{
               location_name = response.location.name;
-            }            
+            }
             if(response.last_name==undefined){
               last_name = '';
             }else{
               last_name = response.last_name;
-            }            
-            if(response.email==undefined){
-              email = '';
-            }else{
-              email = response.email;
-            }            
+            }
+
             if(response.gender==undefined){
               gender = '';
             }else{
               gender = response.gender;
-            }            
+            }
             if(response.birthday==undefined){
               birthday = '';
             }else{
               birthday = response.birthday;
-            }            
-
+            }
+            if(response.email==undefined){
+              email = '<input type="text" name="email" placeholder="ingresa tu correo electronico" class="span9 required email">';
+            }else{
+              email = response.email;
+            }
             $('#register').append('<input type="hidden" name="name" value="'+first_name+'">');
             $('#register').append('<input type="hidden" name="location" value="'+location_name+'">');
             $('#register').append('<input type="hidden" name="lastName" value="'+last_name+'">');
@@ -78,20 +79,11 @@
             $('#facebook-name').html(first_name);
             $('#facebook-birth').html(birthday);
             $('#facebook-email').html(email);
+            showInfo();
           }
         }
-      });
+      })
       $('#fb-show-sheet').remove();
-      $('#btn-login').slideUp('fast');
-      $('.confirm').slideDown('fast');
-      // cargar datos de usuario visibles //
-    
-      if(response.email==undefined){
-        email = '<input type="text" name="email" placeholder="ingresa tu correo electronico" class="span9 required email">';
-      }else{
-        email = response.email;
-      }
-
       $('#facebook-avatar').attr('src','https://graph.facebook.com/'+response.id+'/picture?width=200&amp;height=200');
     }, {scope: 'email,user_friends,friends_birthday,user_birthday,user_location,friends_location,user_interests,user_photos'});
   }
@@ -100,3 +92,8 @@
      e.preventDefault();
      $('#register').submit(); 
   })
+
+  function showInfo(){
+      $('#btn-login').slideUp('fast');
+      $('.confirm').slideDown('fast');
+  }
