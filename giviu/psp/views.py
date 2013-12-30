f0e710711803cd33adfb8dc55a62a03f613fadb4
Rodrigo Valin <licorna@gmail.com>
2013-12-29 23:46:16 -0300

    Added draft Punto Pagos implementation.

--- giviu/psp/views.py
+++ giviu/psp/views.py
@@ -1,20 +1,40 @@
 from django.shortcuts import render, redirect, render_to_response
 from django.http import HttpResponseBadRequest
+from django.conf import settings
+from giviu.models import PaymentTransaction
+import django.core.exceptions.MultipleObjectsReturned
 import random
+
+if settings.DEVELOPMENT:
+    from settings_development import *
+else:
+    from settings_production import *
 
 def first_stage(request):
     if request.method != 'POST':
         return HttpResponseBadRequest()
 
-    print request.POST
+    if 'token' not in request.POST:
+        return HttpResponseBadRequest()
 
-    if random.randint(0,10) == 6:
-        data = {
-            'additional_head': '<meta http-equiv="refresh" content="10; url=http://giviudev.dev:8000/giftcard/error" />'
-        }
-        return render_to_reponse('error.html', data)
+    if 'trx_id' not in request.POST:
+        return HttpResponseBadRequest()
+
+    token = request.POST['token']
+    trx_id = request.POST['trx_id']
+    redirect_to = PUNTO_PAGOS_PHASE3_URL + '/transaccion/procesar/' + token
+    redirect_head = '<meta http-equiv="refresh" content="10; %s" />'
+
+    try:
+        #TODO: Log
+        payment = PaymentTransaction.objects.get(transaction_uuid=trx_id)
+    except MultipleObjectsReturned:
+        #TODO: Error grave
+        pass
+
+    last_state = payment.set_state('CLIENT_BEING_SENT_TO_PP')
 
     data = {
-        'additional_head': '<meta http-equiv="refresh" content="10; url=http://giviudev.dev:8000/giftcard/success" />'
+        'additional_head': redirect_head % (redirect_to, )
     }
     return render_to_response('success.html', data)
