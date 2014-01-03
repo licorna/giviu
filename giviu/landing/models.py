b82a2ceea4a33f0c147d7d274dff9b4b7083df86
Rodrigo Valin <licorna@gmail.com>
2014-01-03 13:03:17 -0300

    Improved beta users registration.

--- giviu/landing/models.py
+++ giviu/landing/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from giviu.utils import get_now
 
-# Create your models here.
+
+class BetaRegisteredUser(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=80, blank=True)
+    email = models.EmailField(max_length=254, unique=True)
+    ip = models.CharField(max_length=15, blank=True)
+    created = models.DateTimeField(default=lambda: get_now())
+
+    class Meta:
+        db_table = 'beta_registered_user'
--- giviu/landing/views.py
+++ giviu/landing/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render_to_response
+from django.http import HttpResponseBadRequest
+from django.template import RequestContext
+from landing.models import BetaRegisteredUser
 
-# Create your views here.
 
 def home(request):
     data = {}
+    if request.method == 'POST':
+        if 'name' not in request.POST or 'email' not in request.POST:
+            return HttpResponseBadRequest()
 
-    return render_to_response('index.html')
+        email = request.POST['email']
+        name = request.POST['name']
+        ip = request.META['REMOTE_ADDR']
+        try:
+            user = BetaRegisteredUser.objects.get(email__exact=email)
+            data['user_already_exist'] = True
+            return render_to_response('langing.html',
+                                      data,
+                                      context_instance=RequestContext(request))
+        except BetaRegisteredUser.DoesNotExist:
+            user = BetaRegisteredUser(
+                email=email,
+                name=name,
+                ip=ip
+            )
+            user.save()
+            data['user_created'] = True
+
+    return render_to_response('landing.html',
+                              data,
+                              context_instance=RequestContext(request))
