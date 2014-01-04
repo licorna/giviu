8a2d101f347d3df684bda7a36be25fc9d3260c69
Rodrigo Valin <licorna@gmail.com>
2014-01-03 22:22:27 -0300

    Added /status/version for getting deployed commit.

--- giviu/status/urls.py
+++ giviu/status/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    url(r'^', 'status.views.index', name='status'),
+    url(r'^$', 'status.views.status', name='status'),
+    url(r'^version$', 'status.views.version', name='version'),
 )
--- giviu/status/views.py
+++ giviu/status/views.py
@@ -1,19 +1,28 @@
 from django.http import HttpResponse
 import json
 from django.conf import settings
+import subprocess
 
 
 def get_git_index_sha():
-    return '57141b8'
+    return subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip()
 
 
-def index(request):
+def status(request):
     data = {
-        'git_index_sha': get_git_index_sha(),
         'database': {
             'name': settings.DATABASES['default']['NAME'],
             'host': settings.DATABASES['default']['HOST']
         },
         'status': 'ok'
+    }
+    return HttpResponse(json.dumps(data), content_type='application/json')
+
+
+def version(request):
+    data = {
+        'git': {
+            'commit': get_git_index_sha()
+        }
     }
     return HttpResponse(json.dumps(data), content_type='application/json')
