9c724ec3c2557ee4a1c87c7ccc7939c5bd95272e
Rodrigo Valin <licorna@gmail.com>
2014-01-13 20:24:28 -0300

    Added resource for fetching friend likes.

--- giviu/social/models.py
+++ giviu/social/models.py
@@ -38,16 +38,17 @@ class Likes():
             return 0
         return None
 
-
     @staticmethod
     def does_user_likes(user, giftcard):
+        '''Returns True if user already likes giftcard,
+        False on contrary.'''
         condition = {
             "giftcard_likes": giftcard,
             "fbid": user,
         }
         url = settings.SOCIAL['ENDPOINT'] + Likes.ENDPOINT
         url += '?where=' + json.dumps(condition)
-        headers = {'Accept':'application/json'}
+        headers = {'Accept': 'application/json'}
         try:
             response = requests.get(url, headers=headers)
         except requests.exceptions.RequestException:
@@ -55,14 +56,13 @@ class Likes():
 
         if response.status_code == 200:
             jres = response.json()
-            if '_items' in jres:
-                return True
+            return '_items' in jres
 
         return False
 
-
     @staticmethod
     def add_giftcard_like(user, giftcard):
+        '''Adds a giftcard_like to given user/giftcard.'''
         condition = '?where={"fbid":"%s"}' % (user,)
         url = settings.SOCIAL['ENDPOINT'] + Likes.ENDPOINT + condition
         headers = {
@@ -102,3 +102,53 @@ class Likes():
         if response.status_code == 200:
             return True
         return False
+
+    @staticmethod
+    def get_social_user(fbid):
+        '''Returns the full user Mongo object.'''
+        condition = {
+            "fbid": fbid,
+        }
+        url = settings.SOCIAL['ENDPOINT'] + Likes.ENDPOINT
+        url += '?where=' + json.dumps(condition)
+        headers = {
+            'Accept': 'applicaton/json'
+        }
+        try:
+            response = requests.get(url, headers=headers)
+        except requests.exceptions.RequestException:
+            return None
+
+        return response.json()
+
+    @staticmethod
+    def get_likes_from_friends(fbid, giftcard):
+        '''Returns a list of facebook ids of friends that liked
+        the given giftcard.'''
+        likes = []
+        condition = {
+            "fbid": fbid,
+            "giftcard_likes": int(giftcard)
+        }
+        url = settings.SOCIAL['ENDPOINT'] + Likes.ENDPOINT
+        url += '?where=' + json.dumps(condition)
+        headers = {'Accept': 'application/json'}
+        friends = Likes.get_social_user(fbid)['_items'][0]['friend_of']
+        for friend in friends:
+            print 'requesting friend:', friend
+            condition = {
+                "fbid": friend,
+                "giftcard_likes": int(giftcard)
+            }
+            url = settings.SOCIAL['ENDPOINT'] + Likes.ENDPOINT
+            url += '?where=' + json.dumps(condition)
+            headers = {'Accept': 'application/json'}
+            response = requests.get(url, headers=headers)
+            if response.status_code == 200:
+                response = response.json()
+                if len(response['_items']) > 0:
+                    if giftcard in response['_items'][0]['giftcard_likes']:
+                        print 'user', friend, 'likes gfs:', response['_items'][0]['giftcard_likes']
+                        likes.append(friend)
+
+        return likes
