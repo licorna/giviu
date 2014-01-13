e64f345d75b2d1941af9dfc20e3ad44be1b8f2e4
Rodrigo Valin <licorna@gmail.com>
2014-01-12 22:06:58 -0300

    Add Like Social Prototype.

--- giviu/social/models.py
+++ giviu/social/models.py
@@ -1,16 +1,20 @@
 import requests
 from django.conf import settings
+import json
 
 
 class Likes():
     ENDPOINT = '/friend'
     TIMEOUT = settings.SOCIAL['TIMEOUT']
 
-    @classmethod
-    def get_giftcard_likes(cls, giftcard_id, just_count=True):
-        condition = '?where={"giftcard_likes":%d}' % (giftcard_id,)
-        url = settings.SOCIAL['ENDPOINT'] + Likes.ENDPOINT + condition
-        print url
+    @staticmethod
+    def get_giftcard_likes(giftcard_id, just_count=True):
+        '''Will fetch all the likes a giftcard has'''
+        condition = {
+            "giftcard_likes": giftcard_id
+        }
+        url = settings.SOCIAL['ENDPOINT'] + Likes.ENDPOINT
+        url += "?where=" + json.dumps(condition)
         headers = {
             'Accept': 'application/json'
         }
@@ -33,3 +37,44 @@ class Likes():
         if just_count:
             return 0
         return None
+
+    @staticmethod
+    def add_giftcard_like(user, giftcard):
+        condition = '?where={"fbid":"%s"}' % (user,)
+        url = settings.SOCIAL['ENDPOINT'] + Likes.ENDPOINT + condition
+        headers = {
+            'Accept': 'application/json'
+        }
+
+        try:
+            response = requests.get(url, data={}, headers=headers,
+                                    timeout=Likes.TIMEOUT)
+        except requests.exceptions.RequestException:
+            return None
+
+        user_likes = []
+        user_id = ''
+        if response.status_code == 200:
+            jres = response.json()
+            if 'giftcard_likes' in jres['_items'][0]:
+                user_likes = jres['_items'][0]['giftcard_likes']
+            user_id = jres['_items'][0]['_id']
+            user_likes.append(int(giftcard))
+
+        url = settings.SOCIAL['ENDPOINT'] + Likes.ENDPOINT
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        data = {
+            'giftcard_likes': user_likes,
+        }
+        try:
+            url = url + '/' + user_id
+            response = requests.patch(url, data=json.dumps(data),
+                                      headers=headers, timeout=Likes.TIMEOUT)
+        except requests.exceptions.RequestException:
+            return None
+
+        if response.status_code == 200:
+            return True
+        return False
