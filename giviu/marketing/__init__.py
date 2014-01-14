5bee9f672b8bfd461f56ae95b19184ff75ca5b80
Rodrigo Valin <licorna@gmail.com>
2014-01-14 19:21:54 -0300

    Added missing marketing events.

--- giviu/marketing/__init__.py
+++ giviu/marketing/__init__.py
@@ -19,6 +19,66 @@ def event_user_registered(email, name):
     msg.send()
 
 
-def event_user_buy_product(email_from, email_to):
-    '''This event gets triggered when the user buys a giftcard.'''
-    pass
+def event_user_buy_product_confirmation(email, **kwargs):
+    '''This event gets triggered when the user buys a giftcard.
+    At the same time he sees the Purchase Confirmation page.'''
+    c = Context(kwargs)
+    html_content = get_template('marketing_buy_confirmation.html').render(c)
+    text_content = get_template('marketing_buy_confirmation.text').render(c)
+    if settings.DEBUG:
+        email = 'licorna@gmail.com'
+    msg = EmailMultiAlternatives('Confirmacion de compra en Giviu',
+                                 text_content,
+                                 settings.EMAIL_DEFAULT_FROM,
+                                 [email])
+    msg.attach_alternative(html_content, 'text/html')
+    msg.send()
+
+
+def event_user_receives_product(email, **kwargs):
+    '''This is the email a receiving users gets when he receives
+    a giftcard indicating on how to proceed.'''
+    c = Context(kwargs)
+    html_content = get_template('marketing_giftcard.html').render(c)
+    text_content = get_template('marketing_giftcard.text').render(c)
+    if settings.DEBUG:
+        email = 'licorna@gmail.com'
+    msg = EmailMultiAlternatives('Has recibido una giftcard!',
+                                 text_content,
+                                 settings.EMAIL_DEFAULT_FROM,
+                                 [email])
+    msg.attach_alternative(html_content, 'text/html')
+    msg.send()
+
+
+def event_user_confirmation_sends_giftcard(email, **kwargs):
+    '''This event triggers when a giftcard a users bought is send to
+    the destination party.'''
+    c = Context(kwargs)
+    html_content = get_template('marketing_sender_notification.html').render(c)
+    text_content = get_template('marketing_sender_notification.text').render(c)
+    if settings.DEBUG:
+        email = 'licorna@gmail.com'
+    msg = EmailMultiAlternatives('Has enviado una giftcard!',
+                                 text_content,
+                                 settings.EMAIL_DEFAULT_FROM,
+                                 [email])
+    msg.attach_alternative(html_content, 'text/html')
+    msg.send()
+
+
+def event_merchant_notification_giftcard_was_bought(email, **kwargs):
+    '''This event triggers when a users buys a giftcard. At the same time
+    the user receives the Purchase Confirmation email, the merchant gets
+    this email confirming a new Giftard was bought.'''
+    c = Context(kwargs)
+    html_content = get_template('marketing_merchant_notification.html').render(c)
+    text_content = get_template('marketing_merchant_notification.text').render(c)
+    if settings.DEBUG:
+        email = 'licorna@gmail.com'
+    msg = EmailMultiAlternatives('Has recibido una giftcard!',
+                                 text_content,
+                                 settings.EMAIL_DEFAULT_FROM,
+                                 [email])
+    msg.attach_alternative(html_content, 'text/html')
+    msg.send()
