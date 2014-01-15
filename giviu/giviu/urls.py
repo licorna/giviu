653a00d494cc7b00d1405cb11a88942eb0e6f086
Rodrigo Valin <licorna@gmail.com>
2014-01-15 00:12:20 -0300

    Fix.

--- giviu/giviu/urls.py
+++ giviu/giviu/urls.py
@@ -4,12 +4,12 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'giviu.views.home', name='home'),
+    url(r'^$', 'giviu.views.home', name='landing_home'),
     url(r'^giftcard/detail/(?P<gift_id>\d+)$', 'giviu.views.giftcard_detail', name='giftcard_detail'),
     url(r'^giftcard/category/(?P<slug>[a-z-]+)$', 'giviu.views.home', name='giftcard_category'),
     url(r'^giftcard/custom/(?P<gift_id>\d+)$', 'giviu.views.giftcard_custom', name='giftcard_custom'),
     url(r'^logout$', 'giviu.views.do_logout', name='logout'),
-    url(r'^register$', 'giviu.views.do_register', name='register'),
+    url(r'^register', 'giviu.views.do_register', name='register'),
     url(r'^giftcard/checkout$', 'giviu.views.giftcard_confirmation', name='giftcard_confirmation'),
     url(r'^product/show/(?P<uuid>\S+)$', 'giviu.views.product_show', name='product_show'),
 
